88ec984058cd6172563243b76f4b0ed8400938ee
snowbytes <84378360+snowbytes@users.noreply.github.com>
2026-01-24 13:08:53 +0100

    feat(client): display server health

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return <main>Howdy</main>;
+  const [health, setHealth] = useState("")
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_ORIGIN}/api/v1/health`)
+      .then(response => response.json())
+      .then(data => setHealth(data))
+      .catch(err => console.error(err))
+  }, [])
+
+  return (
+    <main>
+      Howdy
+      <pre>{JSON.stringify(health)}</pre>
+    </main>
+  )
 }
