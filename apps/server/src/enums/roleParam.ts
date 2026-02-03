82f1aaa8461fa1526f065489c8305d216f3c5cfe
Nishikino Maki <maki.keep@gmail.com>
2026-02-03 21:55:56 +0000

    refactor(server): refactor getFlashcards to filter by role query param

---
+++ apps/server/src/enums/roleParam.ts
@@ -0,0 +1,8 @@
+// query parameter values allowed for role key in request
+export enum RoleParam {
+  SCRUM_PRODUCT_OWNER = "scrum_product_owner",
+  SCRUM_MASTER = "scrum_master",
+  UI_UX_DESIGNER = "ui_ux_designer",
+  WEB_DEVELOPER = "web_developer",
+  PYTHON_DEVELOPER = "python_developer",
+}
