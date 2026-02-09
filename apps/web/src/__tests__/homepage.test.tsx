7604cf57426e2578b3c14fb745d0d02a93b01e97
absterr <absterr.dev@gmail.com>
2026-02-09 20:14:45 +0100

    feat(web): remove navbar from questions page

--- apps/web/src/__tests__/homepage.test.tsx
+++ apps/web/src/__tests__/homepage.test.tsx
@@ -1,8 +1,8 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import Home from '../app/page'
+import Home from "@/app/(main)/page";
+import { render, screen } from "@testing-library/react";
+import { expect, test } from "vitest";
 
 test('Home renders "Howdy"', () => {
-    render(<Home />)
-    expect(screen.getByText('Howdy')).toBeDefined()
-})
+  render(<Home />);
+  expect(screen.getByText("Howdy")).toBeDefined();
+});
--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,14 +1,13 @@
-import type { Metadata } from 'next';
-import { Montserrat } from 'next/font/google';
-import './globals.css';
-import Header from '@/components/header';
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Confido ',
-  description: 'An interview question generator',
+  title: "Confido ",
+  description: "An interview question generator",
 };
 
-const montserrat = Montserrat({ subsets: ['latin'] });
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -17,8 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${montserrat.className} antialiased`}>
-        <Header />{children}</body>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
     </html>
   );
 }
