4ff7f4eb0d7f770863db6c4c2ab51324164709da
absterr <absterr.dev@gmail.com>
2026-02-12 16:55:57 +0100

    feat(web): add warning icon
    Add warning icon and button color to error page

--- apps/web/src/app/questions/error.tsx
+++ apps/web/src/app/questions/error.tsx
@@ -1,21 +1,25 @@
 "use client";
+import WarningIcon from "@/components/icons/WarningIcon";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Error({ error }: { error: Error }) {
   return (
-    <div className="px-4 min-h-screen flex flex-col gap-y-6 items-center justify-center mx-auto max-w-2xl">
-      <p>An unexpected error occured: {error.message}</p>
-      <div className="flex gap-x-4">
+    <main className="px-4 min-h-screen flex flex-col gap-y-6 justify-center mx-auto max-w-2xl">
+      <section className="flex flex-col gap-y-8 items-center justify-center">
+        <WarningIcon />
+        <p>An unexpected error occured: {error.message}</p>
+      </section>
+      <nav className="flex gap-x-4 justify-center">
         <Link href={"/"}>
-          <Button className="p-5">Go home</Button>
+          <Button className="p-5 bg-[#7CA8F9]">Go home</Button>
         </Link>
         <Link href={"/roles"}>
           <Button variant={"outline"} className="p-5">
             See roles
           </Button>
         </Link>
-      </div>
-    </div>
+      </nav>
+    </main>
   );
 }
