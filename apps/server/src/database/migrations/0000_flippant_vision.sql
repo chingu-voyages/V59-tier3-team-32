adc1c15733d60c9c7363b6db29618cd401c6f7f6
Nishikino Maki <maki.keep@gmail.com>
2026-02-20 20:24:59 +0000

    feat(server): add attempts schema

--- apps/server/src/database/migrations/0000_flippant_vision.sql
+++ apps/server/src/database/migrations/0000_flippant_vision.sql
@@ -2,3 +2,12 @@ CREATE TABLE "infra_bootstrap" (
 	"id" text PRIMARY KEY NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL
 );
+
+CREATE TABLE "attempts" (
+	"user_id" text NOT NULL,
+	"quiz_id" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"role" text NOT NULL,
+	"questions_count" integer NOT NULL,
+	"correct_count" integer NOT NULL
+);
