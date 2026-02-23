d638076e78deba45d4e4781f1002f5815ae8aa96
snowbytes <84378360+snowbytes@users.noreply.github.com>
2026-02-23 17:44:47 +0000

    fix(auth): fix attempt for cross domain cookies

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -3,6 +3,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { AUTH_ENV, SERVER_ENV } from "../config/env.js";
 import { makeDb } from "../database/index.js";
 import { account, session, user, verification } from "../database/schema.js";
+import { oAuthProxy } from "better-auth/plugins";
 
 const {
   GITHUB_CLIENT_ID,
@@ -25,6 +26,31 @@ export const auth = betterAuth({
       maxAge: 5 * 60, // 5 MINS
     },
   },
+  // ...
+  // 1. set cross domain cookies config in our backend's better auth config:
+  advanced: {
+    cookies: {
+      /// only changes cookie rules for 'state' cookies (social signin uses those)
+      state: {
+        attributes: {
+          sameSite: "none",
+          secure: true,
+          partitioned: true, // this potentially causes the same issue, try with & without
+        },
+      },
+    },
+  },
+  // ...
+  // 2. setup oauth proxy
+  plugins: [
+    oAuthProxy({
+      productionURL: "https://v59-tier3-team-32-server.vercel.app",
+      // apparently setting this is not needed and will be inferred
+      currentURL: "https://v59-tier3-team-32-web.vercel.app",
+    }),
+  ],
+  // ...
+  // 3. explicitly set the redirectURI
   socialProviders: {
     github: {
       clientId: GITHUB_CLIENT_ID,
@@ -33,6 +59,8 @@ export const auth = betterAuth({
     google: {
       clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
+      redirectURI:
+        "https://v59-tier3-team-32-server.vercel.app/api/auth/callback/google",
     },
   },
 });
